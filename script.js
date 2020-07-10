651d61619deccb579e6a3775fcb1b14d990ed285
nkleinmann <nekleinmann@gmail.com>
2020-07-09 21:30:05 -0400

    Worked out conditionals for length of password

--- script.js
+++ script.js
@@ -14,11 +14,22 @@ function writePassword() {
 // Add event listener to button and storing user input in variables via prompts
 generateBtn.addEventListener("click", promptMe);
 
+
+
+
 function promptMe() {
   let passwordLength = prompt("What is the length of your password?");
-  if ((passwordLength < 8) || (passwordLength > 128));
-    passwordLength = prompt("Please enter a valid length for the password with at least 8 characters and no more than 128 characters.");
+  if ((passwordLength < 8) || (passwordLength > 128)) {
+    alert("Please enter a valid length for the password with at least 8 characters and no more then 128 characters.");
+  }
+  if (isNaN(passwordLength)) {
+    alert("Please enter a valid numerical length for the password with at least 8 characters and no more than 128 characters.")
+  }
   return passwordLength;
+
+  // if ((passwordLength < 8) || (passwordLength > 128));
+  //   passwordLength = prompt("Please enter a valid length for the password with at least 8 characters and no more than 128 characters.");
+  // return passwordLength;
 
     // const passwordLength = prompt("What is the length of your password?");
     // switch (passwordLength) {
