cc8a810af9b587234f65d2486f2a68800e9b6600
nkleinmann <nekleinmann@gmail.com>
2020-07-10 18:45:50 -0400

    Updated function to make user choose at least one option for password

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ const speCharArr = speChar.split("");
 // console.log(speCharArr);
 let passArray = [];
 
+
+
+
 let letters = " "
 let passwd = []
 
@@ -30,10 +33,13 @@ function writePassword() {
  // Add event listener to button and storing user input in variables via prompts
  generateBtn.addEventListener("click", function() {
     pwLength();
-    lowerCase();
-    upperCase();
-    numberCase();
-    specialCase();
+    passwordOptions();
+
+    // lowerCase();
+    // upperCase();
+    // numberCase();
+    // specialCase();
+
   });
 
 
@@ -45,48 +51,82 @@ function pwLength() {
   let passwordLength = prompt("What is the length of your password?");
   if ((passwordLength < 8) || (passwordLength > 128)) {
     alert("Please enter a valid length for the password with at least 8 characters and no more then 128 characters.");
+    return passwordLength;
   }
   // isNan() returns true when input is not a number
   else if (isNaN(passwordLength)) {
     alert("Please enter a valid numerical length for the password with at least 8 characters and no more than 128 characters.")
   }
-  return passwordLength;
-    } 
-
-// Adds lower case letters to array if selected
-function lowerCase() {
+}
+function passwordOptions() {
   let lCase = confirm("Do you want lower case letters in your password?");
+  let uCase = confirm("Do you want upper case letters in your password?");
+  let numCase = confirm("Do you want numbers in your password?");
+  let spCase = confirm("Do you want special characters in your password?");
   if (lCase === true) {
     passArray = passArray.concat(lowerArr)
-    console.log(passArray);
+    // console.log(passArray);
   }
-}
-
-// Adds upper case letters to array if selected
-function upperCase() {
- let uCase = confirm("Do you want upper case letters in your password?");
- if (uCase === true) {
-   passArray = passArray.concat(upperArr);
-   console.log(passArray);
- } 
-}
-
-// Adds numbers to array if selected
-function numberCase() {
-  let numCase = confirm("Do you want numbers in your password?");
-  if (numCase === true) {
+  else if (uCase === true) {
+    passArray = passArray.concat(upperArr);
+     //  console.log(passArray);
+    } 
+  else if (numCase === true) {
     passArray = passArray.concat(numArr);
-    console.log(passArray);
-  } 
- }
-
- // Adds special characters to array if selected
- function specialCase() {
-  let spCase = confirm("Do you want special characters in your password?");
-  if (spCase === true) {
+      // console.log(passArray);
+    }
+  else if (spCase === true) {
     passArray = passArray.concat(speCharArr);
-    console.log(passArray);
-  } 
- }
+      // console.log(passArray);
+    }
+    else {
+      alert("Please chose at least one of the options for your password.");
+    }
+  }
+
+// // Adds lower case letters to array if selected
+// function lowerCase() {
+//   // let lCase = confirm("Do you want lower case letters in your password?");
+//   if (lCase === true) {
+//     passArray = passArray.concat(lowerArr)
+//     // console.log(passArray);
+//     return lCase;
+//   }
+// }
+
+// // Adds upper case letters to array if selected
+// function upperCase() {
+// //  let uCase = confirm("Do you want upper case letters in your password?");
+//  if (uCase === true) {
+//    passArray = passArray.concat(upperArr);
+//   //  console.log(passArray);
+//   return uCase;
+//   }
+//  } 
+
+// // Adds numbers to array if selected
+// function numberCase() {
+//   // let numCase = confirm("Do you want numbers in your password?");
+//   if (numCase === true) {
+//     passArray = passArray.concat(numArr);
+//     // console.log(passArray);
+//     return numCase;
+//   }
+//  }
+
+//  // Adds special characters to array if selected
+// function specialCase() {
+//   // let spCase = confirm("Do you want special characters in your password?");
+//   if (spCase === true) {
+//     passArray = passArray.concat(speCharArr);
+//     // console.log(passArray);
+//     return spCase;
+//    }
+//   } 
+
+  
 
 
+ //   // if ((lCase === false) && (uCase === false) && (numCase === false) && (spCase === false)) {
+//   //   
+//   //  
