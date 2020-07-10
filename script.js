0fdef95ed9c51dad073ac0f7d206178b51175912
nkleinmann <nekleinmann@gmail.com>
2020-07-09 22:45:55 -0400

    Added arrays of lower and upper case letters, numbers, and special chars

--- script.js
+++ script.js
@@ -1,5 +1,20 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
+const lower = "abcdefghijklmnopqrstuvwxyz";
+const lowerArr = lower.split("");
+// console.log(lowerArr);
+const upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const upperArr = upper.split("");
+// console.log(upperArr);
+const num = "0123456789";
+const numArr = num.split("");
+// console.log(numArr);
+const speChar = "!#$%&'()*+,'()*+,-./:;<=>?@[\]^_`{|}~"
+const speCharArr = speChar.split("");
+// console.log(speCharArr);
+
+let letters = " "
+let passwd = []
 
 // Write password to the #password input
 function writePassword() {
@@ -12,19 +27,35 @@ function writePassword() {
 
 
 // Add event listener to button and storing user input in variables via prompts
-generateBtn.addEventListener("click", pwLength);
+generateBtn.addEventListener("click", function() {
+  pwLength();
+  lowerCase();
+});
 
 
 
 
+// Function prompts user for length of password and alerts if password length is outside of restraints
 function pwLength() {
   let passwordLength = prompt("What is the length of your password?");
   if ((passwordLength < 8) || (passwordLength > 128)) {
     alert("Please enter a valid length for the password with at least 8 characters and no more then 128 characters.");
   }
+  // isNan() returns true when input is not a number
   else if (isNaN(passwordLength)) {
     alert("Please enter a valid numerical length for the password with at least 8 characters and no more than 128 characters.")
   }
   return passwordLength;
     } 
+
+// function lowerCase() {
+//   lCase = confirm("Do you want lower case letters in your password?");
+//   if (lCase === true) {
+//     const l = range(97, 122);
+//     String.fromCharCode(l);
+//     let lLetters = letters.split(" ");
+//     console.log(lLetters);
+//   }
+
+// }
 
