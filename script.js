32fc532d48af6f983c255d1a6ce4cc571b896811
nkleinmann <nekleinmann@gmail.com>
2020-07-11 14:51:32 -0400

    Updated Comments

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 
- // Add event listener to button and storing user input in variables via prompts
+ // Add event listener to button and calling function
  generateBtn.addEventListener("click", function() {
   passwordDecisions();
   });
 
-// Function prompts user for length of password and alerts if password length is outside of restraints
+// Function for prompts and adding password to textvalue
 function passwordDecisions() {
 
-  // Assignment Code for strings and arrays
+// Assignment Code for strings and arrays
   let passwd = [];
   let passArray = [];
   const lower = "abcdefghijklmnopqrstuvwxyz";
@@ -24,31 +24,35 @@ function passwordDecisions() {
   const speChar = "!#$%&'()*+,'()*+,-./:;<=>?@[\]^_`{|}~"
   const speCharArr = speChar.split("");
 // console.log(speCharArr);
-// let passArray = [];
-// let finalPassword = " ";
-  // password length
 
+// Resets final password
   let finalPassword = "";
 
+  // Write password to the #password input
   let passwordText = document.querySelector("#password");
   passwordText.value = finalPassword;
 
+  // password length 
   let passwordLength = prompt("What is the length of your password?");
   if ((passwordLength < 8) || (passwordLength > 128)) {
     alert("Please enter a valid length for the password with at least 8 characters and no more then 128 characters.");
     return;
   }
+ 
+  // password length must be a valid numberical length
   // isNan() returns true when input is not a number (checks user input is a number)
   if (isNaN(passwordLength)) {
     alert("Please enter a valid numerical length for the password with at least 8 characters and no more than 128 characters.");
     return;
   }
+  
   // Declaring variables for other 4 password options
   const lCase = confirm("Do you want lower case letters in your password?");
   const uCase = confirm("Do you want upper case letters in your password?");
   const numCase = confirm("Do you want numbers in your password?");
   const spCase = confirm("Do you want special characters in your password?");
-  // Addomg lower case, upper case, numbers, and special characters to arrays
+  
+  // Adding lower case, upper case, numbers, and special characters to arrays
   if (lCase === true) {
     passArray = passArray.concat(lowerArr)
     // console.log(passArray);
@@ -65,11 +69,12 @@ function passwordDecisions() {
     passArray = passArray.concat(speCharArr);
       // console.log(passArray);
     }
-    // Alerting user that they must choose at least one of the options for a password
+  // Alerting user that they must choose at least one of the options for a password
   if ((lCase === false) && (uCase === false) && (numCase === false) && (spCase === false)) {
       alert("Please chose at least one of the options for your password.");
     }
     // console.log(passArray)
+  
     // for loop to create a random password with the correct length and options
   const passwordArrayLength = passArray.length;
   for (let i = 0; i < passwordLength; i++) {
@@ -80,8 +85,7 @@ function passwordDecisions() {
   finalPassword = passwd.join('');
   // console.log(finalPassword)
   
-  // Write password to the #password input
-  // let passwordText = document.querySelector("#password");
+  // Writes new password to the #password input
   passwordText.value = finalPassword;
 }
 
