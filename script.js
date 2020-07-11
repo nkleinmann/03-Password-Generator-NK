3cda5c1e82846a2ef73ded719dde1e2fb35d9526
nkleinmann <nekleinmann@gmail.com>
2020-07-10 21:25:53 -0400

    Added return so if user enters length outside of constraints, have to restart

--- script.js
+++ script.js
@@ -33,7 +33,8 @@ function writePassword() {
  // Add event listener to button and storing user input in variables via prompts
  generateBtn.addEventListener("click", function() {
     pwLength();
-    passwordOptions();
+    // passwordOptions();
+    // getRandom();
 
     // lowerCase();
     // upperCase();
@@ -51,36 +52,66 @@ function pwLength() {
   let passwordLength = prompt("What is the length of your password?");
   if ((passwordLength < 8) || (passwordLength > 128)) {
     alert("Please enter a valid length for the password with at least 8 characters and no more then 128 characters.");
-    return passwordLength;
+    return;
   }
   // isNan() returns true when input is not a number
-  else if (isNaN(passwordLength)) {
-    alert("Please enter a valid numerical length for the password with at least 8 characters and no more than 128 characters.")
+  if (isNaN(passwordLength)) {
+    alert("Please enter a valid numerical length for the password with at least 8 characters and no more than 128 characters.");
+    return;
   }
-}
-function passwordOptions() {
   let lCase = confirm("Do you want lower case letters in your password?");
   let uCase = confirm("Do you want upper case letters in your password?");
   let numCase = confirm("Do you want numbers in your password?");
   let spCase = confirm("Do you want special characters in your password?");
   if (lCase === true) {
     passArray = passArray.concat(lowerArr)
-    // console.log(passArray);
+    console.log(passArray);
   }
-  else if (uCase === true) {
+  if (uCase === true) {
     passArray = passArray.concat(upperArr);
-     //  console.log(passArray);
+     console.log(passArray);
     } 
-  else if (numCase === true) {
+  if (numCase === true) {
     passArray = passArray.concat(numArr);
-      // console.log(passArray);
+      console.log(passArray);
     }
-  else if (spCase === true) {
+  if (spCase === true) {
     passArray = passArray.concat(speCharArr);
-      // console.log(passArray);
+      console.log(passArray);
     }
-    else {
+  else {
       alert("Please chose at least one of the options for your password.");
     }
   }
 
+// function passwordOptions() {
+//   let lCase = confirm("Do you want lower case letters in your password?");
+//   let uCase = confirm("Do you want upper case letters in your password?");
+//   let numCase = confirm("Do you want numbers in your password?");
+//   let spCase = confirm("Do you want special characters in your password?");
+//   if (lCase === true) {
+//     passArray = passArray.concat(lowerArr)
+//     console.log(passArray);
+//   }
+//   if (uCase === true) {
+//     passArray = passArray.concat(upperArr);
+//      console.log(passArray);
+//     } 
+//   if (numCase === true) {
+//     passArray = passArray.concat(numArr);
+//       console.log(passArray);
+//     }
+//   if (spCase === true) {
+//     passArray = passArray.concat(speCharArr);
+//       console.log(passArray);
+//     }
+//   else {
+//       alert("Please chose at least one of the options for your password.");
+//     }
+//   }
+
+// function getRandom() {
+//   for (let i = 0; i < pwLength; i ++) {
+//     passwd = passArray[Math.floor(Math.random()*passArray.length)];
+//   }
+// }
